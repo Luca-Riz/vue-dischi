ee67a1e2aabdcd78d0bedbccf0c19890a3334b0a
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-08 16:21:33 +0200

    fixed padding Album

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,25 @@
 
     <div class="container">
         <div class="row row-cols-2 row-cols-md-4 row-cols-lg-5">
-          <div class="col m-2">
+          <div class="col px-4 my-2">
             <Album />
           </div>
-          <div class="col m-2">
+          <div class="col px-4 my-2">
             <Album />
           </div>
-          <div class="col m-2">
+          <div class="col px-4 my-2">
             <Album />
           </div>
-          <div class="col m-2">
+          <div class="col px-4 my-2">
             <Album />
           </div>
-          <div class="col m-2">
+          <div class="col px-4 my-2">
             <Album />
           </div>
-          <div class="col m-2">
+          <div class="col px-4 my-2">
             <Album />
           </div>
-          <div class="col m-2">
+          <div class="col px-4 my-2">
             <Album />
           </div>
 
